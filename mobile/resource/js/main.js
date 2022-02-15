7f47d02ae1383f9220aa16ac0bb6b3ac2ace55a2
SJ1986 <qkrtjdwo1986@gmail.com>
2022-02-15 16:25:09 +0900

    20220215

--- mobile/resource/js/main.js
+++ mobile/resource/js/main.js
@@ -1,8 +1,19 @@
 $(function () {
-    thumbListHover();
+    mainSlide();
 });
 
-function thumbListHover(){
-    
-
+function mainSlide(){
+    var mainVisual = new Swiper(".main-visual .swiper-container", {
+        slidesPerView: 1,
+        pagination: {
+            el: ".main-visual .swiper-pagination",
+            type:"fraction",
+        },
+    });
+    var top10Slide = new Swiper('.top10 .swiper-container', {
+        slidesPerView:'auto',
+    });
+    var campusSlide = new Swiper('.campusSlide .swiper-container', {
+        slidesPerView:'auto',
+    });
 }
