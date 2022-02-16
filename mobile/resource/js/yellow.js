23e2ea9e7c5c8fbb2c98097bf5e991f609b95f31
hyunjoo2 <ttldlguswn@gmail.com>
2022-02-16 17:08:39 +0900

    [yellow] 220216 자유게시판, 모달

--- mobile/resource/js/yellow.js
+++ mobile/resource/js/yellow.js
@@ -1,5 +1,7 @@
 $(function () {
     scrapOn();
+    linkPlus(); // 링크 추가 버튼 클릭
+    filePlus(); // 파일첨부 버튼 클릭
 });
 
 function scrapOn(){
@@ -7,3 +9,42 @@ function scrapOn(){
        $(this).toggleClass("on");
    })
 }
+function linkPlus(){
+
+    $(".click-plusLink").on("click",function(){
+        var $formClone = $(this).parent().prev().children('.form-input:last');
+        var $clone = $formClone.clone().addClass("clone");
+
+        $cloneInput = $('<div class="form-input d-flex align-items-center"><input type="text" name="searchKeyword" value="" id="" class="form-input w-600" placeholder="URL을 입력해 주세요" /><button class="button-delete"></button></div>').addClass("clone");
+        $formClone.after($cloneInput);
+
+        
+        // 삭제
+        $(".button-delete").on("click",function(){
+			var $delInput = $(this).parents('.form-input.clone');
+			$delInput.remove();
+		});
+    });
+  
+
+ }
+
+ function filePlus(){
+
+    $(".click-plusFile").on("click",function(){
+        var $formClone2 = $(this).parent().prev().children('.form-input:last');
+        var $clone2 = $formClone2.clone().addClass("clone");
+
+        $cloneInput2 = $('<div class="form-input d-flex align-items-center"><input type="text" name="searchKeyword" value="" id="" class="form-input w-600" placeholder="파일을 추가해 주세요." /><button class="button-delete"></button></div>').addClass("clone");
+        $formClone2.after($cloneInput2);
+
+        
+        // 삭제
+        $(".button-delete").on("click",function(){
+			var $delInput2 = $(this).parents('.form-input.clone');
+			$delInput2.remove();
+		});
+    });
+  
+
+ }
