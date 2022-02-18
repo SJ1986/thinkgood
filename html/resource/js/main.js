9170ec4e6643f634f55c626f8e95d8ffbf0f8bd1
SJ1986 <qkrtjdwo1986@gmail.com>
2022-02-18 21:53:48 +0900

    0218

--- html/resource/js/main.js
+++ html/resource/js/main.js
@@ -4,6 +4,20 @@ $(function () {
 });
 
 function thumbListHover(){
-    
-
+    $('.list-thumb.pick li').hover(function(){
+        $(this).toggleClass('on')
+        if($(this).hasClass('on')){
+            $(this).find('img').attr('src','./resource/image/main/img-hoverImg-01.jpg');
+        }else{
+            $(this).find('img').attr('src','./resource/image/sub/img-contestThumb02.png');
+        }
+    })
+    $('.list-thumb.activity li').hover(function(){
+        $(this).toggleClass('on')
+        if($(this).hasClass('on')){
+            $(this).find('img').attr('src','./resource/image/main/img-hoverImg-02.jpg');
+        }else{
+            $(this).find('img').attr('src','./resource/image/sub/img-contestThumb03.png');
+        }
+    })
 }
