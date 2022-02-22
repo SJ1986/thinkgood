b5aadce03d372bd863b4e505d06a27f2398552d3
sunmi <red@wordncode.com>
2022-02-22 16:35:22 +0900

    red_20220222

--- mobile/resource/js/red.js
+++ mobile/resource/js/red.js
@@ -1,11 +1,26 @@
 $(function () {
+    projectClick();
     pwCheck();
+    genderButton();
+   
 });
 
+function projectClick(){
+    var item = $('.board-write .tab-content .mody_items .items');
+    item.click(function(){
+        $(this).toggleClass('selection');
+    });
+}  
+function genderButton(){
+    $('.formGender button').click(function(){
+        $(this).removeClass('on');
+        $(this).addClass('on');
+    });
+}    
 function pwCheck(){
     $('.modal-footer .cancel').click(function(){
         $('.modal.myRoom_apply').removeClass('modal-open');
         $('body').removeClass('modal-open');
         $('.modal-backdrop').removeClass('show'); 
     });
-}    
+}   
