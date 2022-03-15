59a71a58747ebe32699fac7aa3667f38721f91f1
SJ1986 <qkrtjdwo1986@gmail.com>
2022-03-15 18:08:11 +0900

    0315

--- html/resource/js/main.js
+++ html/resource/js/main.js
@@ -6,18 +6,18 @@ $(function () {
 function thumbListHover(){
     $('.list-thumb.pick li').hover(function(){
         $(this).toggleClass('on')
-        if($(this).hasClass('on')){
-            $(this).find('img').attr('src','./resource/image/main/img-hoverImg-01.jpg');
-        }else{
-            $(this).find('img').attr('src','./resource/image/sub/img-contestThumb02.png');
-        }
+        // if($(this).hasClass('on')){
+        //     $(this).find('img').attr('src','./resource/image/main/img-hoverImg-01.jpg');
+        // }else{
+        //     $(this).find('img').attr('src','./resource/image/sub/img-contestThumb02.png');
+        // }
     })
     $('.list-thumb.activity li').hover(function(){
         $(this).toggleClass('on')
-        if($(this).hasClass('on')){
-            $(this).find('img').attr('src','./resource/image/main/img-hoverImg-02.jpg');
-        }else{
-            $(this).find('img').attr('src','./resource/image/sub/img-contestThumb03.png');
-        }
+        // if($(this).hasClass('on')){
+        //     $(this).find('img').attr('src','./resource/image/main/img-hoverImg-02.jpg');
+        // }else{
+        //     $(this).find('img').attr('src','./resource/image/sub/img-contestThumb03.png');
+        // }
     })
 }
